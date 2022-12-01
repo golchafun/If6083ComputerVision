cf914745caf258586f8fd895ddaa3a5a56396a50
golchafun <wannabe.golcha@gmail.com>
2022-12-01 10:02:47 +0700

    finish eksperimen hw2

--- tryhw2.py
+++ tryhw2.py
@@ -2,29 +2,34 @@ from uwimg import *
 im = load_image("data/dog.jpg")
 f = make_box_filter(7)
 blur = convolve_image(im, f, 1)
-save_image(blur, "hasil/dog-box7")
+save_image(blur, "dog-box7")
 
+im = load_image("data/dog.jpg")
+f = make_box_filter(7)
+blur = convolve_image(im, f, 1)
+thumb = nn_resize(blur, blur.w//7, blur.h//7)
+save_image(thumb, "dogthumb")
 
 im = load_image("data/dog.jpg")
 f = make_gaussian_filter(2)
 blur = convolve_image(im, f, 1)
-save_image(blur, "hasil/dog-gauss2")
+save_image(blur, "dog-gauss2")
 
 im = load_image("data/dog.jpg")
 f = make_gaussian_filter(2)
 lfreq = convolve_image(im, f, 1)
 hfreq = im - lfreq
 reconstruct = lfreq + hfreq
-save_image(lfreq, "hasil/low-frequency")
-save_image(hfreq, "hasil/high-frequency")
-save_image(reconstruct, "hasil/reconstruct")
+save_image(lfreq, "low-frequency")
+save_image(hfreq, "high-frequency")
+save_image(reconstruct, "reconstruct")
 
 im = load_image("data/dog.jpg")
 res = sobel_image(im)
 mag = res[0]
 feature_normalize(mag)
-save_image(mag, "hasil/magnitude")
+save_image(mag, "magnitude")
 
 im = load_image("data/dog.jpg")
 colorize_image = colorize_sobel(im)
-save_image(colorize_image, "hasil/colorize_dog")
+save_image(colorize_image, "colorize_dog")
